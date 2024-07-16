f6cb5c43bf4918df802ab504b455b0a03d827a64
yanggenjie <yang_genjie@126.com>
2024-07-16 17:45:18 +0800

    fix: 抽象

--- InstallAndRemoveAddin/Programs.cs
+++ InstallAndRemoveAddin/Programs.cs
@@ -9,26 +9,31 @@ namespace InstallAndRemoveAddin
 {
     public class Program
     {
+        static string addinName;
+        /// <summary>
+        /// exe和addin模板结构：exe所在文件夹为当前文件夹,./data/xxx.addin
+        /// 输入三个参数：参数1：addin文件名，不带后缀；参数2：install或者uninstall，参数3：revit版本:2020、2021等数字
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            if (args.Length != 1) return;
-            if (args[0] == "install")
+            if (args.Length != 3) return;
+            addinName = args[0];
+            if (string.IsNullOrWhiteSpace(addinName)) return;
+
+            if (args[1] == "install")
             {
-                CopyAddinFile();
+                CopyAddinFile(args[2]);
             }
             else if (args[0] == "uninstall")
             {
-                DeleteAddinFile();
+                DeleteAddinFile(args[2]);
             }
         }
-        const string addinName = "RebarSkeleton.addin";
 
-        static void CopyAddinFile()
+        static void CopyAddinFile(string version)
         {
             string curPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
-            // 当前版本
-            var version = Path.GetFileName(curPath);
 
             // 当前addin模板文件
             string addinFileName = Path.Combine(curPath, "data", addinName);
@@ -51,12 +56,11 @@ namespace InstallAndRemoveAddin
             File.WriteAllText(destAddinFileName, content);
         }
 
-        static void DeleteAddinFile()
+        static void DeleteAddinFile(string version)
         {
             string curPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             // 当前版本
-            var version = Path.GetFileName(curPath);
             string addinFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\Autodesk\\Revit\\Addins\\{version}";
             string destAddinFileName = Path.Combine(addinFolder, addinName);
             if (File.Exists(destAddinFileName))
